b18e593ae0f8ff2ab11a127da7914f45fcafef2f
Seth Huffman <sethwhuffman@gmail.com>
2019-01-24 20:49:03 -0600

    Add files via upload

---
+++ webpack-demo/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+ const webpack = require('webpack');
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+  module.exports = {
+    entry: './src/index.js',
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Caching'
+      }),
+      new webpack.HashedModuleIdsPlugin()
+    ],
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    }
+  };
